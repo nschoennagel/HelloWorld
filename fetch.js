9643c384118ac60c72f81de8c4c8d219a95fca12
Nico <nschoennagel@oev.de>
2021-03-25 17:50:37 +0100

    initialize

---
+++ fetch.js
@@ -0,0 +1,13 @@
+
+window.onload = function() {
+    document.getElementById('button').addEventListener('click', function () {
+        fetch('https://4fbk09atjk.execute-api.eu-west-1.amazonaws.com/prod/sltutorial?key1=Nico')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myContent) => {
+            document.querySelector('.content').innerHTML = myContent;
+        });
+    
+    }, false);
+}
