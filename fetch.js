ddaa72bf7b9b64b6f0b4733cdb40c1f3f7c36109
S0581565 <Rishi.Srinivasan@Student.HTW-Berlin.de>
2021-11-19 13:50:01 +0100

    Additional textboxes added for accepting keys

--- fetch.js
+++ fetch.js
@@ -1,7 +1,10 @@
 
 window.onload = function() {
     document.getElementById('button').addEventListener('click', function () {
-        fetch('https://4fbk09atjk.execute-api.eu-west-1.amazonaws.com/prod/sltutorial?key1=Nico')
+        let val1 = document.querySelector('input[id="myvalue1"]').value;
+        let val2 = document.querySelector('input[id="myvalue2"]').value;
+        console.log(val1);
+        fetch('https://pusvrdh7y8.execute-api.eu-central-1.amazonaws.com/default/myFirstWebsite?key1='+val1+'&key2='+val2)
         .then((response) => {
             return response.text();
         })
